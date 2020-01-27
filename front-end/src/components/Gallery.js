a771d0b227ff29ccea8727e4bae86e4d38306c55
Maleeha <malehahasan@gmail.com>
2020-01-27 13:33:11 -0500

    Gallery component complete

--- front-end/src/components/Gallery.js
+++ front-end/src/components/Gallery.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { FaInstagram } from 'react-icons/fa'
 
-function Gallery() {
-  return (
-    <div>
-      <div className="Divider1"></div><div className="Divider2"></div>
+class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: "https://i.imgur.com/A48gNu6.jpg"
 
-      <div className="Gal">
-        <div className="GalleryHeading"></div>
-        <p className="Gallery"> Gallery<FaInstagram className="icons" /></p>
-        <div className="GalleryHeading"></div >
-      </div>
-    </div >
-  )
+    }
+  }
+
+  handleClick = (e) => {
+    this.setState({
+      image: e.target.src
+    })
+  }
+
+  render() {
+    return (
+      <div className="gallery-container" >
+        <div className="Divider1"></div> <div className="Divider2"></div>
+        <div className="Gal">
+          <div className="GalleryHeading"></div>
+          <p className="Gallery"> Gallery<FaInstagram className="icons" /></p>
+          <div className="GalleryHeading"></div >
+        </div>
+        <img src={this.state.image} alt="food" className="selected-image"></img>
+        <div className="gallery-images">
+          <img src="https://i.imgur.com/A48gNu6.jpg" alt="food" value="https://i.imgur.com/A48gNu6.jpg" className="gallery-image-container" onClick={(e) => this.handleClick(e)} ></img>
+          <img src="https://i.imgur.com/WksUOzz.jpg" alt="food" value="https://i.imgur.com/WksUOzz.jpg" className="gallery-image-container" onClick={(e) => this.handleClick(e)} ></img>
+          <img src="https://i.imgur.com/LsgTuQ4.jpg" alt="food" value="https://i.imgur.com/LsgTuQ4.jpg" className="gallery-image-container" onClick={(e) => this.handleClick(e)} ></img>
+          <img src="https://i.imgur.com/dKHM2g6.jpg" alt="food" value="https://i.imgur.com/dKHM2g6.jpg" className="gallery-image-container" onClick={(e) => this.handleClick(e)} ></img>
+          <img src="https://i.imgur.com/IX9jIQk.jpg" alt="food" value="https://i.imgur.com/IX9jIQk.jpg" className="gallery-image-container" onClick={(e) => this.handleClick(e)} ></img>
+          <img src="https://i.imgur.com/fTNhbiz.jpg" alt="food" value="https://i.imgur.com/fTNhbiz.jpg" className="gallery-image-container" onClick={(e) => this.handleClick(e)} ></img>
+        </div >
+      </div >
+    )
+  }
 }
 
 export default Gallery
